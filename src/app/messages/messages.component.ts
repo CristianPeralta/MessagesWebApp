1e65a0908c1e403b242b46ea2b1bfbf8082f0d33
CristianPeralta <cbperaltasegura@gmail.com>
2019-12-16 17:07:00 -0500

    show messages

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-
+  messages = [
+    {
+      id: 0,
+      username: 'Albert',
+      text: 'This is a message',
+    },
+    {
+      id: 1,
+      username: 'Rous',
+      text: 'Hello guys',
+    },
+    {
+      id: 2,
+      username: 'Luka',
+      text: 'Hi, I\'m Luka',
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
